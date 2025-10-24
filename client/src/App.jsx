de42b459b9716131e95bc017107e868323437678
Abdelrahman Adel Mekky <abdalrahman_adel201@outlook.com>
2025-10-24 15:56:33 +0300

    Update dependencies and refactor App component
    - Added new dependencies: axios, date-fns, and lucide-react to package.json and package-lock.json.
    - Refactored App component to include routing with React Router, integrating JobList and Layout components.
    - Simplified CSS for the root element to ensure full width.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { JobList } from './pages/JobList';
+import { Layout } from './components/layout/Layout';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Layout> 
+        <Routes>
+
+          <Route path="/" element={<JobList />} />
+
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
